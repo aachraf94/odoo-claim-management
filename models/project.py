c82344e1df4f077688c4b1917303fe9a97e04901
Abdelkebir Achraf <104693339+aachraf94@users.noreply.github.com>
2025-01-27 19:49:08 +0100

    refactor: remove unused claim project action and streamline project views

--- models/project.py
+++ models/project.py
@@ -9,14 +9,3 @@ class ClaimProject(models.Model):
         ('commercial', 'Commercial'),
         ('technical', 'Technical')
     ], string='Claim Type')
-
-    def action_view_claim(self):
-        self.ensure_one()
-        return {
-            'name': 'Claim',
-            'type': 'ir.actions.act_window',
-            'res_model': 'claim.claim',
-            'view_mode': 'form',
-            'res_id': self.claim_id.id,
-            'target': 'current',
-        }
